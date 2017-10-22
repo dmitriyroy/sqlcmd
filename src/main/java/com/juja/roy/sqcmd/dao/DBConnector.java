9429564c192783e4bd749bfc0591d89933c8a5e2
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-10-22 23:01:55 +0300

    убрал геттер для пароля

--- src/main/java/com/juja/roy/sqcmd/dao/DBConnector.java
+++ src/main/java/com/juja/roy/sqcmd/dao/DBConnector.java
@@ -82,9 +82,9 @@ public class DBConnector {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
-    }
+//    public String getPassword() {
+//        return password;
+//    }
 
     public void setPassword(String password) {
         this.password = password;
